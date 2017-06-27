79edf74ce259da5490113425056f7f5188af7bd2
mclinch <michael.clinch@gmail.com>
2017-06-26 21:10:26 -0700

    Create BasicCard.js

---
+++ BasicCard.js
@@ -0,0 +1,5 @@
+The constructor should accept two arguments:'front' and 'back'.
+
+  * The constructed object should have a 'front' property that contains the text on the front of the card.
+
+  * The constructed object should have a 'back' property that contains the text on the back of the card.
